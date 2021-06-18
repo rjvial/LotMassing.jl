6cc1c46d3f8a47bea44fa861163970e56f796947
Raimundo Vial <rjvial@gmail.com>
2021-06-18 09:27:13 -0400

    cambios menores

--- src/script_nlo_evol.jl
+++ src/script_nlo_evol.jl
@@ -16,7 +16,7 @@ using LotMassing, .poly2D, .polyShape, CSV, JLD2, FileIO
 ##############################################
 
 idPredio = 1 # 8 predio = 1,2,3,4,5,6,7,9
-conjuntoTemplates = [5] # 4 [1:L, 2:C, 3:lll, 4:V, 5:H]
+conjuntoTemplates = [2] # 4 [1:L, 2:C, 3:lll, 4:V, 5:H]
 
 dirTerrenos = string(pwd(), "\\", "src", "\\")
 
